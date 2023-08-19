881f8783ed5891f832dfb12ce7e207ec65c62ebe
shamita123 <142479223+shamita123@users.noreply.github.com>
2023-08-19 02:38:39 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array{
+    public static void main(String[] arg){
+
+ String a ="sham";
+
+ String name[] ={"sham","ram","sheeta","gita"};
+ System.out.println(name[1]);
+    }
+}
+
+        
+
