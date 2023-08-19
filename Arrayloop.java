eeadcc552890b7bbb920f264f09d7c74672e39e0
shamita123 <142479223+shamita123@users.noreply.github.com>
2023-08-19 02:39:38 -0700

    Add files via upload

---
+++ Arrayloop.java
@@ -0,0 +1,14 @@
+public class Array{
+    public static void main(String[] arg){
+
+        String name[] = {"sham","ram","sheeta","gita"};
+
+        int number [] = {23,49,46,38};
+        
+        System.out.println(number[3]);
+        for(int i=0 ; i<=number.length;i++){
+            System.out.println(number[i]);
+        }
+    }
+}
+
